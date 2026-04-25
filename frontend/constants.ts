0bbe430d12565a96dad7ab1a3d7098441bbe437f
unknown <ketan.gupta1412@gmail.com>
2026-04-25 18:01:26 +0530

    Added missing package

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,10 +1,11 @@
 const DEFAULT_API_BASE_URL = "https://websockets-base.onrender.com/";
 const DEFAULT_WS_BASE_URL = "ws://websockets-base.onrender.com/ws";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL;
-export const WS_BASE_URL =
-  import.meta.env.VITE_WS_BASE_URL || DEFAULT_WS_BASE_URL;
+export const API_BASE_URL = DEFAULT_API_BASE_URL;
+  // import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL;
+
+export const WS_BASE_URL = DEFAULT_WS_BASE_URL;
+  // import.meta.env.VITE_WS_BASE_URL || DEFAULT_WS_BASE_URL;
 
 // Exponential backoff configuration
 export const MAX_RECONNECT_DELAY = 30000; // 30 seconds
